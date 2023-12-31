a0b4ea99d5d78ef6963a53f836302fee59067488
sthiven <sthiven.correia@global.ntt>
2023-12-31 19:01:10 -0300

    :sparkles: appweb bolao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,114 @@
+'use client'
+import axios from 'axios';
 import Image from 'next/image'
-
+import { useEffect, useState } from 'react';
+const jogos = [
+  [3, 7, 14, 17, 19, 21, 22, 24, 26, 28, 30, 32, 52, 55],
+  [9, 11, 14, 17, 26, 31, 32, 40, 46, 54],
+  [5, 6, 9, 12, 21, 36, 48, 50, 57],
+  [4, 10, 21, 40, 43, 51, 59, 60],
+  [1, 6, 13, 16, 21, 24, 49],
+  [7, 13, 22, 23, 45, 47, 52],
+  [3, 16, 23, 34, 35, 54, 60],
+  [4, 6, 21, 28, 34, 45],
+  [4, 7, 9, 13, 31, 57],
+  [14, 22, 25, 39, 45, 52],
+  [11, 15, 17, 29, 51, 52]
+]
+const acertos: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+const numerosSorteados = [1, 5, 7, 9, 0, 3]
 export default function Home() {
+  const [reload, setReload] = useState(true)
+  const [resultados, setResultados] = useState<number[]>([]);
+  const [refresh, setRefresh] = useState(false)
+  const [premios, setPremios] = useState<{
+    "descricao": string,
+    "faixa": number,
+    "ganhadores": number,
+    "valorPremio": number
+  }[]>([]);
+  const fetchData = async () => {
+    try {
+      setReload(true)
+      const response = await axios.get('https://loteriascaixa-api.herokuapp.com/api/megasena/latest');
+      setReload(false)
+      setResultados(response.data.dezenasOrdemSorteio.map((number: string) => Number(number)));
+      setPremios(response.data.premiacoes)
+    } catch (error) {
+      console.error('Erro ao obter resultados:', error);
+    } finally {
+      // Configura a próxima chamada após 10 segundos, garantindo que não acumule chamadas se a resposta demorar
+      setTimeout(fetchData, 10000);
+    }
+  };
+  useEffect(() => {
+    fetchData()
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex w-full h-full flex-col items-center">
+      <section className='flex flex-col w-full items-center my-10'>
+        <h4 className='flex items-center gap-4 '>
+          Numeros sorteados {reload ?
+            <div className='w-4 h-4 rounded-full border-4 border-gray-600' />
+            :
+            null
+          }
+        </h4>
+        <div className='flex gap-4 font-bold text-green-400 text-2xl'>
+
+          {resultados.map(number => <div>{number}</div>)}
         </div>
-      </div>
+      </section>
+      <section className='grid grid-cols-1 gap-2 md:grid-cols-3 w-full place-items-center my-4'>
+        {premios.map((premio) => {
+          const acertosNumber = parseInt(premio.descricao.match(/\d+/)?.[0] || '6', 10);
+          const ganhamu = acertos.find(number => number >= acertosNumber) !== undefined
+          return (
+            <article className={`${ganhamu ? 'bg-blue-400 ' : 'bg-gray-700'} rounded-lg p-4 w-48`}>
+              <h4 className={`w-full text-center  ${ganhamu ? 'bg-blue-950 text-white' : 'bg-slate-950 text-gray-400'} rounded py-1`}>{premio.descricao}</h4>
+              <div>
+                Faixa: {premio.faixa}
+              </div>
+              <div>
+                Ganhadores: {premio.ganhadores}
+              </div>
+              <div>
+                Premio: R${premio.valorPremio}
+              </div>
+            </article>
+          )
+        })}
+      </section>
+      <section className='grid grid-cols-2 lg:grid-cols-11 md:grid-cols-7 sm:grid-cols-5 w-full place-items-center px-10 justify-around items-center'>
+        {jogos.map((jogo, index) => {
+          const numeroDeAcertos = jogo.filter(valor => resultados.includes(valor));
+          acertos[index] = numeroDeAcertos.length
+          return (
+            <article className='text-center'>
+              <h3>Acertos</h3>
+              <h4>{acertos[index]}</h4>
+              <div className='grid grid-cols-2 gap-4'>
+                {jogo.map((Number) => {
+                  return (
+                    <div
+                      className={`
+                      text-center 
+                      rounded 
+                      px-2 py-1 
+                      text-white
+                      ${resultados.includes(Number) ? 'bg-blue-400' : 'bg-gray-500'}
+                    `}>
+                      {Number}
+                    </div>)
+                })}
+              </div>
+            </article>
+          )
+        })}
+      </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   )
 }
